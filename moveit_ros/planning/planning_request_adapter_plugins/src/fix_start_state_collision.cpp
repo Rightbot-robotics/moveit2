33b522d7df07e0c49f502b520bce3ba70bdd1220
Inderjeet Yadav <inderjeet.yadav@rightbot.in>
2023-10-22 15:48:53 +0530

    check for NaN in start state cc

--- moveit_ros/planning/planning_request_adapter_plugins/src/fix_start_state_collision.cpp
+++ moveit_ros/planning/planning_request_adapter_plugins/src/fix_start_state_collision.cpp
@@ -84,7 +84,17 @@ public:
     collision_detection::CollisionRequest creq;
     creq.group_name = req.group_name;
     collision_detection::CollisionResult cres;
-    planning_scene->checkCollision(creq, cres, start_state);
+
+    cres.collision = false;
+    Eigen::VectorXd joint_positions; 
+    // TODO: use current planning group instead of a const value
+    const std::string planning_group = "arm";
+    start_state.copyJointGroupPositions(start_state.getJointModelGroup(planning_group), joint_positions);
+  
+    if(!joint_positions.hasNaN()) {
+      planning_scene->checkCollision(creq, cres, start_state);
+    }
+
     if (cres.collision)
     {
       // Rerun in verbose mode
